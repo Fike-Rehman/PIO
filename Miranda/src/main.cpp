44ca2c9b7b6f99347a7e3b591c17478493fbea86
Fike Rehman <fike.rehman@hotmail.com>
2025-09-11 19:42:58 -0500

    connect to wifi and diplay time

--- Miranda/src/main.cpp
+++ Miranda/src/main.cpp
@@ -21,20 +21,71 @@ Miranda:
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <config.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// ====== Function prototypes =====
+void connectWifi();
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -18000, 60000); // UTC -5 hours for Minneapolis, update every 60 seconds
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(100);
+
+ connectWifi();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// ===== Function definitions =====
+void connectWifi() {
+  Serial.println();
+  Serial.println("Booting...");
+  Serial.println("Connecting to: " + String(WiFiSSID));
+
+  // Start WiFi connection
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  Serial.print("Connecting to WiFi ");
+  Serial.print(WiFiSSID);
+
+  // Wait for connection
+  uint8_t retries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    retries++;
+    if (retries > 60) {  // 30 sec timeout
+      Serial.println("\nFailed to connect to WiFi!");
+      return;
+    }
+  }
+
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+
+  Serial.print("Signal Strength (RSSI): ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
+
+  // Initialize and get time
+  timeClient.begin();
+  if (timeClient.forceUpdate()) {
+    Serial.print("Current time (CST): ");
+    Serial.println(timeClient.getFormattedTime());
+  } else {
+    Serial.println("Failed to get time from NTP server");
+  }
+  
 }
